8c98c3694669e03ad40abe3c216a133e9bbeb61c
philwing100 <philwing100@gmail.com>
2024-07-11 14:43:11 -0500

    MDN part 5 completed all read only parts of site are done

--- express-locallibrary-tutorial/populatedb.js
+++ express-locallibrary-tutorial/populatedb.js
@@ -1,7 +1,9 @@
 #! /usr/bin/env node
 
 console.log(
-    'This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: node populatedb "mongodb+srv://cooluser:coolpassword@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority"'
+    'This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: '+
+    'node populatedb "mongodb+srv://cooluser:coolpassword@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority"'
+    //                mongodb+srv://philwing100:TkDBUq99aDoh7oh0@cluster0.iqoodpk.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0
   );
   
   // Get arguments passed on command line
@@ -11,6 +13,8 @@ console.log(
   const Author = require("./models/author");
   const Genre = require("./models/genre");
   const BookInstance = require("./models/bookinstance");
+  //const mongooser = require('./mongooser');
+
   
   const genres = [];
   const authors = [];
